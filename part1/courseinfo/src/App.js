eba2b01aa858c0007884610ed3144d2e3678666a
Chris Morin <longhumansbiz@gmail.com>
2022-11-17 16:23:30 -0400

    part1 courseinfo

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+const Header = (props) => {
+  return (
+    <div>
+      {props.name}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part.name} {props.part.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  debugger
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content parts={course.parts} />
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+    </div>
+  );
+}
+
+export default App;
